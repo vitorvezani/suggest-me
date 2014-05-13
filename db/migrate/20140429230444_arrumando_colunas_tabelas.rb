74842e2ca3436e03a41840e2db9061dfc7700aba
Vitor Vezani <vitor.vezani@gmail.com>
2014-05-13 17:57:52 -0300

    [13/05/2014] Mudanças para produção.

--- db/migrate/20140429230444_arrumando_colunas_tabelas.rb
+++ db/migrate/20140429230444_arrumando_colunas_tabelas.rb
@@ -3,7 +3,7 @@ class ArrumandoColunasTabelas < ActiveRecord::Migration
   	 # avaliacoes
   	rename_column :avaliacoes, :id_user, :id_usuario
   	rename_column :avaliacoes, :id_tipo_avaliacao, :avaliacao
-  	change_column(:avaliacoes, "avaliacao", "int", limit: 1 )
+  	change_column(:avaliacoes, "avaliacao", "integer", limit: 1 )
   	# usuarios
   	rename_column :usuarios, :role, :admin
   	change_column(:usuarios, "admin", "boolean", default: false)
