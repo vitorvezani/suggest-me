b7a8341dbc202093e4d80619a486629943936dad
Vitor Vezani <vitor.vezani@gmail.com>
2014-05-13 17:52:39 -0300

    [13/05/2014] Alterações para produção

--- db/migrate/20140425215052_senha_unique_false.rb
+++ db/migrate/20140425215052_senha_unique_false.rb
@@ -1,7 +1,5 @@
 class SenhaUniqueFalse < ActiveRecord::Migration
   def change
   	change_column(:usuarios, "senha", "string", {limit: 40, unique: false, null: false})
-  	execute "ALTER TABLE `itens` ADD UNIQUE `page_id_nome_ptbr_id_categoria` (`nome_ptbr`,`id_categoria`)"
-  	execute "ALTER TABLE `itens` ADD UNIQUE `page_id_nome_en_id_categoria` (`nome_en`,`id_categoria`)"
   end
 end
